283a04e936f4f4d9fe69c5cecd0b01a3eacc3c89
Yves Jaques <yves.jaques@asksuite.com>
2025-04-29 21:48:32 -0300

    Route Links & Relative Links

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -18,7 +18,7 @@ export const routes: Routes = [
                 component: TasksComponent,
             },
             {
-                path: 'new',
+                path: 'tasks/new',
                 component: NewTaskComponent,
             },
         ]
